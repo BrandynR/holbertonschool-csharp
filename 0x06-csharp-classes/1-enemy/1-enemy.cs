f4f7bf555b6545bb2a187e89c86e3379a0664f79
BrandynR <brandynr@github.com>
2020-04-30 00:48:49 -0400

    write a public class Zombie that defines a zombie

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Enemies
+{
+	///<summary>Creating zombie class.</summary>
+	public class Zombie
+	{
+		///<summary>Public field for health of zombie.</summary>
+		public int health;
+		///<summary>public constructor for zombie.</summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+	}
+}

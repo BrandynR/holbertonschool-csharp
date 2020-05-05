027914c2dc440c84410c7c28287d1a84d873edcf
BrandynR <brandynr@github.com>
2020-05-05 13:24:07 -0400

    Fixed Typo

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -8,21 +8,6 @@ namespace MyMath.Tests
 		[Test]
 		public void Test1()
 		{
-			int[,] matrix = new int[2, 2] { { 6, 2 }, { 2, 4 } };
-			int num = 2;
-
-			int[,] output = Matrix.Divide(matrix, num);
-
-			Assert.AreEqual(new int[2, 2] { { 3, 1 }, { 1, 2 } }, output);
-		}
-        [Test]
-		public void Test2()
-		{
-			Assert.AreEqual(null, Matrix.Divide(null, 2));
-		}
-		[Test]
-		public void Test3()
-		{
 			int[,] matrix = null;
 			int num = 2;
 
@@ -30,15 +15,8 @@ namespace MyMath.Tests
 
 			Assert.AreEqual(null, output);
 		}
-        [Test]
-		public void Test4()
-		{
-			int[,] matrix = new int[2, 2] { { 9, 3 }, { 6, 12 } };
-
-			Assert.AreEqual(null, Matrix.Divide(matrix, 0));
-		}
 		[Test]
-		public void Test5()
+		public void Test2()
 		{
 			int[,] matrix = new int[2, 2] { { 6, 2 }, { 2, 4 } };
 			int num = 0;
@@ -47,12 +25,25 @@ namespace MyMath.Tests
 
 			Assert.AreEqual(null, output);
 		}
-        [Test]
-		public void Test6()
+		[Test]
+		public void Test3()
 		{
-            int[,] matrix = new int[2, 2] { { 0, 3 }, { 6, 12 } };
+			int[,] matrix = new int[2, 2] { { 6, 2 }, { 2, 4 } };
+			int num = 2;
 
-			Assert.AreEqual(new int[2, 2] { { 0, 1 }, { 2, 4 } }, Matrix.Divide(matrix, 3));
+			int[,] output = Matrix.Divide(matrix, num);
+
+			Assert.AreEqual(new int[2, 2] { { 3, 1 }, { 1, 2 } }, output);
+		}
+		[Test]
+		public void Test4()
+		{
+			int[,] matrix = new int[2, 2] { { 9, 3 }, { 6, 12 } };
+			int num = 0;
+			
+			int[,] output = Matrix.Divide(matrix,num);
+			
+			Assert.AreEqual(null, Matrix.Divide(matrix, num));
 		}
 	}
 }

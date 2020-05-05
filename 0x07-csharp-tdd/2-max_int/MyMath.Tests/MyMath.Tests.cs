45624660e37766f6605e05b9fa1268d675caeced
BrandynR <brandynr@github.com>
2020-05-05 13:41:39 -0400

    method that returns the max integer in a list of integers

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,34 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace MyMath.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            List<int> list = new List<int>(new int[]{});
+            int max = Operations.Max(list);
+            
+            Assert.AreEqual(0, max);
         }
+        [Test]
+        public void Test2()
+        {
+            List<int> list = null;
+            int max = Operations.Max(list);
+            
+            Assert.AreEqual(0, max);
+        }
+        [Test]
+		public void Test3()
+		{
+			List<int> list = new List<int>(new int[]{1, 2, 9, 3, 4});
+            int max = Operations.Max(list);
+            
+            Assert.AreEqual(9, max);
+		}
     }
 }

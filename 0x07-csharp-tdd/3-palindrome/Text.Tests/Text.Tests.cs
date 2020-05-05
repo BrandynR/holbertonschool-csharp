0f247ab52668023d1ceb3d02cbad9661343677ea
BrandynR <brandynr@github.com>
2020-05-05 13:51:50 -0400

    method that returns True if a string is a palindrome

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -2,17 +2,54 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+	[TestFixture]
+	public class StrTests
+	{
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+		public void Test1()
+		{
+			string s = "";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+        [Test]
+		public void Test2()
+		{
+			string s = "Racecar";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+		[Test]
+		public void Test3()
+		{
+			string s = "TacoCat";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+
+		[Test]
+		public void Test4()
+		{
+			string s = "starts";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsFalse(output);
+		}
+		[Test]
+		public void Test5()
+		{
+			string s = "A man, a plan, a canal: Panama.";
+
+			bool output = Str.IsPalindrome(s);
+
+			Assert.IsTrue(output);
+		}
+	}
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Text
 {
-    public class Class1
-    {
-    }
+	///<summary>Class for string operations</summary>
+	public class Str
+	{
+		///<summary>check if string s is palindrome and returns true or false</summary>
+		public static bool IsPalindrome(string s)
+		{
+			s = Regex.Replace(s, "[^A-Za-z]", "");
+
+			char[] array = s.ToCharArray();
+			Array.Reverse(array);
+
+			return s.ToLower() == new string(array).ToLower();
+		}
+	}
 }

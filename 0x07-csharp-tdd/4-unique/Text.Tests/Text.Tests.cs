cac333e73d8ac93e4473f4b42424e6426947f358
BrandynR <brandynr@github.com>
2020-05-05 14:15:22 -0400

    method that returns the index of the first non-repeating character of a string

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -2,17 +2,30 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+	///<summary>Unit testing for unique string</summary>
+	[TestFixture]
+	public class StrTexts
+	{
+        ///<summary>Check if the string is empty</summary>
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+		public void Test1()
+		{
+			string st = "";
+
+			int output = Str.UniqueChar(st);
+
+			Assert.AreEqual(-1, output);
+		}
+
+		///<summary>Checks function operation</summary>
+		[Test]
+		public void Test2()
+		{
+			string st = "ssissss";
+
+			int output = Str.UniqueChar(st);
+
+			Assert.AreEqual(2, output);
+		}
+	}
 }
--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -2,7 +2,29 @@
 
 namespace Text
 {
-    public class Class1
-    {
-    }
+	///<summary> string operations</summary>
+	public class Str
+	{
+		///<summary>method that returns the index of the first non-repeating character of a string</summary>
+		public static int UniqueChar(string s)
+		{
+			if (s == "")
+			{
+				return (-1);
+			}
+
+			char x = s[0];
+            var sizing =  s.Length;
+            int index = 0;
+			for (; index < sizing; index++)
+			{
+				if (s[index] != x)
+				{
+					return index;
+				}
+				x = s[index];
+			}
+			return (-1);
+		}
+	}
 }

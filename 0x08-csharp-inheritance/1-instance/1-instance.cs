e7644f9fd9449e3346253f1873ae8c94f0d44611
BrandynR <brandynr@github.com>
2020-05-07 11:16:07 -0400

    True if the object is an instance of, or an instance of a class that inherited from, Array

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> Object class</summary>
+    class Obj
+    {
+
+    /// <summary>method that returns True if the object is an instance of
+    /// or if the object is an instance of a class that inherited from, Array</summary>
+    ///<returns> True or False </returns>
+
+        public static bool IsInstanceOfArray(object obj)
+        {
+            return typeof(Array).IsInstanceOfType(obj);
+        }
+    }

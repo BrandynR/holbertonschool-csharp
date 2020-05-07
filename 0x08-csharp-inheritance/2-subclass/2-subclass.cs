c610fb3faec6db56dea16fe39cc0e491f86cdacd
BrandynR <brandynr@github.com>
2020-05-07 11:20:23 -0400

    checks if object is a subclass

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _2_subclass
-{
-    public class Class1
+    /// <summary> Object class</summary>
+    class Obj
     {
+
+    /// <summary>method that returns True if the object is an instance of a class that
+    /// inherits from the specified class, otherwise return False</summary>
+    ///<returns> True or False </returns>
+
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return derivedType.IsSubclassOf(baseType);
+        }
     }
-}

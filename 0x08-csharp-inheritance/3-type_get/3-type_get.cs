e569ba434cd0cbc52601a41c6b0c9fb8fb52628d
BrandynR <brandynr@github.com>
2020-05-07 11:33:13 -0400

    prints al properties of an object

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
-namespace _3_type_get
-{
-    public class Class1
+
+    /// <summary> Object class</summary>
+    class Obj
     {
+       ///<summary>method that prints the names of the available properties and methods of an object</summary>
+       ///<returns> All properties of an object</returns>
+    public static void Print(object myObj)
+	{
+		TypeInfo idx = myObj.GetType().GetTypeInfo();
+		Console.WriteLine("{0} Properties:", idx.Name);
+		foreach (PropertyInfo prop in idx.GetProperties())
+		{
+			Console.WriteLine(prop.Name);
+		}
+
+		Console.WriteLine("{0} Methods:", idx.Name);
+		foreach (MethodInfo metInfo in idx.GetMethods())
+		{
+			Console.WriteLine(metInfo.Name);
+		}
+	}
     }
-}

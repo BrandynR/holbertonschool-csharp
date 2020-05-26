6288ca029dee9dc872d4c5e7587a44eb0cc9f7b0
BrandynR <brandynr@github.com>
2020-05-25 20:35:33 -0400

    Calculate magnitude for 2d/3d vector

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,18 +1,22 @@
 using System;
 
-/// <summary>Contains methods to operate on vectors.</summary>
+///<summary>Class for VectorMath</summary>
 class VectorMath
 {
-    /// <summary>Find the magnitude of a vector.</summary>
-    /// <param name="vector">The two coordinates of the vector destination, assuming a source of (0, 0[, 0]).</param>
-    /// <returns>The length/magnitude of the vector.</returns>
-    public static double Magnitude(double[] vector) {
-        if (vector is null || vector.Rank != 1 || vector.Length < 2 || vector.Length > 3)
-            return -1;
-        double ret = Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2);
-        if (vector.Length > 2)
-            ret += Math.Pow(vector[2], 2);
-        ret = Math.Sqrt(ret);
-        return ret;
-    }
+	///<summary>calcuate the magnitude for 2d and 3d vector</summary>
+	///<return>the magnitude or -1</return>
+	public static double Magnitude(double[] vector)
+	{
+		if (vector.Length == 2)
+		{
+			double result = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2));
+			return Math.Round(result, 2);
+		}
+		else if (vector.Length == 3)
+		{
+			double result = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2));
+			return Math.Round(result, 2);
+		}
+		return -1;
+	}
 }

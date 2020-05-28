119ad7aba525ba8afdf33bcf86be467d1f07c554
BrandynR <brandynr@github.com>
2020-05-27 20:26:21 -0400

    typo corrected

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -5,30 +5,30 @@ class MatrixMath
 {
 	///<summary>method that rotates a square 2D matrix by a given angle in radians and returns the resulting matrix</summary>
 	///<return>the result matrix or matrix containing -1</return>
+	
 	public static double[,] Rotate2D(double[,] matrix, double angle)
 	{
-		double[,] rotation = {
-			{Math.Cos(angle), -Math.Sin(angle)},
-			{Math.Sin(angle), Math.Cos(angle)}
-		};
-		int mtrx_row = matrix.GetLength(0);
-		int mtrx_col = matrix.GetLength(1);
+		double[,] failMatrix = { { -1 } };
+		double[,] newMatrix = new double[2, 2];
+		double[,] rotateMatrix = { { Math.Cos(angle), Math.Sin(angle) }, { Math.Sin(angle) * -1, Math.Cos(angle) } };
+		int i, j, k;
+		double prod = 0;
 
-		if (2 == mtrx_row)
+		if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+			return (failMatrix);
+
+		for (i = 0; i < 2; i++)
 		{
-			double[,] result = new double[2, mtrx_col];
-			for (int i = 0; i < mtrx_row; i++)
+			for (j = 0; j < 2; j++)
 			{
-				for (int j = 0; j < 2; j++)
+				prod = 0;
+				for (k = 0; k < 2; k++)
 				{
-					for (int k = 0; k < mtrx_col; k++)
-					{
-						result[i, j] += Math.Round(rotation[i, k] * matrix[k, j]);
-					}
+					prod += Math.Round(matrix[i, k] * rotateMatrix[k, j], 2);
 				}
+				newMatrix[i, j] = prod;
 			}
-			return result;
 		}
-		return new double[,] { { -1 } };
+		return (newMatrix);
 	}
 }

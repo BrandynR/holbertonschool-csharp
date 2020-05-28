89df46f4af6c54d9f01713d8dc7a1a830673b2c6
BrandynR <brandynr@github.com>
2020-05-28 13:35:24 -0400

    fixed output

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -5,35 +5,36 @@ using System.Collections.Generic;
 class MatrixMath
 {
     ///<summary>Shear a matrix.</summary>
-    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
-    {
-        double[,] fail = new double[,] {{-1}};
-        double[,] translate = new double[2, 2] {{1, 0}, {0, 1}};
-        double[,] matrix3 = new double[2, 2];
-        double temp;
+   public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+	{
+		double[,] failMatrix = { { -1 } };
+		double[,] newMatrix = new double[2, 2];
+		double[,] shear;
+		int i, j, k;
+		double prod = 0;
 
-        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-        {
-            return (fail);
-        }
-        if (direction == 'x')
-            translate[1, 0] = factor;
-        else if (direction == 'y')
-            translate[0, 1] = factor;
-        else
-            return (fail);
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                temp = 0;
-                for (int k = 0; k < 2; k++)
-                {
-                    temp += matrix[i, k] * translate[k, j];
-                }
-                matrix3[i, j] = Math.Round(temp, 2);
-            }
-        }
-        return (matrix);
-    }
+		if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+			return (failMatrix);
+
+		if (direction == 'x')
+			shear = new double[,] { { 1, 0 }, { factor, 1 } };
+		else if (direction == 'y')
+			shear = new double[,] { { 1, factor }, { 0, 1 } };
+		else
+			return (failMatrix);
+
+		for (i = 0; i < 2; i++)
+		{
+			for (j = 0; j < 2; j++)
+			{
+				prod = 0;
+				for (k = 0; k < 2; k++)
+				{
+					prod += matrix[i, k] * shear[k, j];
+				}
+				newMatrix[i, j] = Math.Round(prod, 2);
+			}
+		}
+		return (newMatrix);
+	}
 }

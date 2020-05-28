a680e32cc4f9d61f3f4369e66625f89bae49533b
BrandynR <brandynr@github.com>
2020-05-28 14:05:44 -0400

    typo

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -7,27 +7,25 @@ class MatrixMath
 	/// <returns>The determinant of the matrix, -1 if not a 2D or 3D matrix.</returns>
 	
     public static double Determinant(double[,] matrix)
-    {
-        double[,] bad = new double[,] {{-1}};
-        double deter = 0;
-        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-        {
-            deter = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
-        }
-        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
-        {
-            det = Math.Round(
-                (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
-                (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) +
-                (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]) -
-                (matrix[0, 0] * matrix[2, 1] * matrix[1, 2]) -
-                (matrix[2, 0] * matrix[1, 1] * matrix[0, 2]) -
-                (matrix[1, 0] * matrix[0, 1] * matrix[2, 2]), 2);
-        }
-        else
-        {
-            return (-1);
-        }
-        return (deter);
-    }
+	{
+		double deter = 0;
+
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+		{
+			deter = Math.Round(
+				(matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+		}
+		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+		{
+			deter = Math.Round(
+				matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[2, 1] * matrix[1, 2])) -
+				matrix[0, 1] * ((matrix[0, 1] * matrix[2, 2]) - (matrix[2, 1] * matrix[0, 2])) +
+				matrix[0, 2] * ((matrix[0, 1] * matrix[1, 2]) - (matrix[1, 1] * matrix[0, 2])), 2);
+		}
+		else
+		{
+			return (-1);
+		}
+		return (deter);
+	}
 }

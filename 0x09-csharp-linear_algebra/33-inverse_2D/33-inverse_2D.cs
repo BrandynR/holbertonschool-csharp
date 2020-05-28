8502669a0af9ed13681e4eafa540550f61cd0529
BrandynR <brandynr@github.com>
2020-05-27 20:59:18 -0400

    typo fix

--- 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -8,21 +8,21 @@ class MatrixMath
 	/// <param name="matrix">The 2D matrix.</param>
 	/// <returns>A new 2D matrix that is the inverse of matrix.</returns>
 	public static double[,] Inverse2D(double[,] matrix)
-	{
-		double[,] failMatrix = new double[,] { { -1 } };
-		double d = 0;
+    {
+        double[,] bad = new double[,] {{-1}};
+        double det;
 
-		if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-			return (failMatrix);
-
-		d = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
-		if (d == 0)
-			return (failMatrix);
-
-		double[,] newMatrix = new double[,] {
-			{Math.Round((1 / d) * matrix[1, 1], 2), Math.Round((1 / d) * -matrix[0, 1], 2)},
-			{Math.Round((1 / d) * -matrix[1, 0], 2), Math.Round((1 / d) * matrix[0, 0], 2)}
-		};
-		return (newMatrix);
-	}
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            if (det == 0)
+                return (bad);
+            double[,] inverse = new double[,] {
+                {(1 / det) * matrix[1, 1], (1 / det) * -matrix[0, 1]},
+                {(1 / det) * -matrix[1, 0], (1 / det) * matrix[0, 0]}
+            };
+            return (inverse);
+        }
+        return (bad);
+    }
 }

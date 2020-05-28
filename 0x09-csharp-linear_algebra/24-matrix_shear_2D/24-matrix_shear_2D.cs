1749c46cad07c00688cd51cf45fb7a89412bcebb
BrandynR <brandynr@github.com>
2020-05-27 20:49:02 -0400

    typo

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,43 +1,44 @@
 using System;
 
+///<summary>Class for matrix math</summary>
 class MatrixMath
 {
-	/// <summary>shears a square 2D matrix by a given shear factor and returns the resulting matrix</summary>
-	/// <param name="matrix">The matrix.</param>
-	/// <param name="direction">The direction to shear.</param>
-	/// <param name="factor">How much to shear by.</param>
-	/// <returns>A new sheared matrix, -1 on fail.</returns>
-
+	///<summary>applying sheer to matrix</summary>
+	///<return>the result matrix or matrix containing -1</return>
 	public static double[,] Shear2D(double[,] matrix, char direction, double factor)
 	{
-		double[,] fail = { { -1 } };
-		double[,] new = new double[2, 2];
-		double[,] shear;
-		int i, j, k;
-		double prod = 0;
-
-		if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
-			return (fail);
-
-		if (direction == 'x')
-			shear = new double[,] { { 1, 0 }, { factor, 1 } };
-		else if (direction == 'y')
-			shear = new double[,] { { 1, factor }, { 0, 1 } };
-		else
-			return (fail);
-
-		for (i = 0; i < 2; i++)
+		if (direction != 'x' || direction != 'y' || matrix.GetLength(0) != 2)
 		{
-			for (j = 0; j < 2; j++)
+			return new double[,] { { -1 } };
+		}
+
+		if (direction != 'x')
+		{
+			double[,] shear = new double[,] {
+			{1, factor},
+			{0, 1}
+			};
+		}
+
+		else if (direction != 'x')
+		{
+			double[,] shear = new double[,] {
+			{1, 0},
+			{factor, 1}
+			};
+		}
+
+		double[,] result = new double[m1_row, m2_col];
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
 			{
-				prod = 0;
-				for (k = 0; k < 2; k++)
+				for (int k = 0; k < 2; k++)
 				{
-					prod += matrix[i, k] * shear[k, j];
+					result[i, j] += shear[i, k] * matrix[k, j];
 				}
-				new[i, j] = Math.Round(prod, 2);
 			}
 		}
-		return (new);
+		return result;
 	}
 }

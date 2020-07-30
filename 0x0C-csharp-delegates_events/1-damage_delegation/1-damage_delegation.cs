34ed23e6827c6dad5dda0742d14f85f3499fa1b0
BrandynR <brandynr@github.com>
2020-07-29 23:16:32 -0400

    ew method HPValueWarning inside the Player class:

--- 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,46 +1,51 @@
 using System;
 
-delegate void CalculateHealth(float health);
-
-///<summary> Player class</summary>
+///<summary>Player class</summary>
 public class Player
 {
     private string name;
     private float maxHp;
     private float hp;
+    ///<summary>Calculate health</summary>
+    public delegate void CalculateHealth(float amount);
 
     ///<summary>Player Class Constructor</summary>
     public Player(string name = "Player", float maxHp = 100f)
     {
-        if (maxHp <= 0f)
+        this.name = name;
+        if (maxHp <= 0)
         {
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
         else
+        {
             this.maxHp = maxHp;
-        this.name = name;
+        }
         this.hp = this.maxHp;
     }
 
     ///<summary>Player health</summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
-    }
-       ///<summary> Will calculate Player damage.</summary>
-    public void TakeDamage(float damage)
-    {
-        Console.WriteLine($"{this.name} takes {damage} damage!");
-        if (damage <= 0f)
-            this.hp -= damage;
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
     }
 
-    ///<summary>Calculate Player healing.</summary>
+    ///<summary>Calculate damage</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            Console.WriteLine(this.name + " takes 0 damage!");
+        else
+            Console.WriteLine(this.name + " takes " + damage + " damage!");
+    }
+
+    ///<summary>Heal / damage</summary>
     public void HealDamage(float heal)
     {
-        Console.WriteLine($"{this.name} heals {heal} HP!");
-           if (this.hp >= 0)
-            this.hp += heal;
+        if (heal < 0)
+            Console.WriteLine(this.name + " heals 0 HP!");
+        else
+            Console.WriteLine(this.name + " heals " + heal + " HP!");
     }
 }
